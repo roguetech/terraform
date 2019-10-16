6a81390c802dbc77bd65b2ae69342b60e2ebdbaf
vagrant <vagrant@localhost.localdomain>
2019-10-16 15:25:42 +0000

    ecs

---
+++ ecs/vars.tf
@@ -0,0 +1,25 @@
+variable "AWS_REGION" {
+	default = "eu-west-1"
+}
+
+variable "PATH_TO_PRIVATE_KEY" {
+	default = "mykey"
+}
+
+variable "PATH_TO_PUBLIC_KEY" {
+	default = "mykey.pub"
+}
+
+variable "ECS_INSTANCE_TYPE" {
+	default = "t2.micro"
+}
+
+variable "ECS_AMIS" {
+	type = map(string)
+	default = {
+		eu-west-1 = "ami-0963349a5568210b8"
+	}
+}
+
+variable "AWS_ACCESS_KEY" {}
+variable "AWS_SECRET_KEY" {}
