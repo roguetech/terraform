6d2862e8de6a7d1b8b76a1d77819b1d050fa5598
Patrick Roughan <padraig.oruchain@gmail.com>
2019-10-23 15:43:58 +0000

    volume add folder

--- elk-ecs/ecs.tf
+++ elk-ecs/ecs.tf
@@ -10,7 +10,7 @@ resource "aws_launch_configuration" "ecs-elk-launchconfig" {
 	key_name	= aws_key_pair.mykeypair.key_name
 	iam_instance_profile	= aws_iam_instance_profile.ecs-ec2-role.id
 	security_groups	= [aws_security_group.ecs-securitygroup.id, aws_security_group.elasticsearch-node-communication.id]
-	user_data	= "#!/bin/bash\necho 'ECS_CLUSTER=elk-cluster' > /etc/ecs/ecs.config\nstart ecs\nsysctl -w vm.max_map_count=262144\ndocker plugin install   --alias cloudstor:aws   --grant-all-permissions   docker4x/cloudstor:17.06.0-ce-aws2         CLOUD_PLATFORM=AWS         AWS_REGION=eu-west-1         EFS_SUPPORTED=0         DEBUG=1"
+	user_data	= "#!/bin/bash\necho 'ECS_CLUSTER=elk-cluster' > /etc/ecs/ecs.config\nstart ecs\nsysctl -w vm.max_map_count=262144\ndocker plugin install   --alias cloudstor:aws   --grant-all-permissions   docker4x/cloudstor:17.06.0-ce-aws2         CLOUD_PLATFORM=AWS         AWS_REGION=eu-west-1         EFS_SUPPORTED=0         DEBUG=1/nmkdir -p /mnt/ebs/esdata/nchmod -R 777 /mnt/ebs/"
         lifecycle {
 		create_before_destroy = true
 	}
@@ -22,7 +22,7 @@ resource "aws_autoscaling_group" "ecs-elk-autoscaling" {
 # aws_subnet.main-public-2.id]
 	launch_configuration	= aws_launch_configuration.ecs-elk-launchconfig.name
 	min_size		= 1
-	max_size		= 2
+	max_size		= 1
 	tag {
 		key		= "Name"
 		value		= "ecs-ec2-container"
