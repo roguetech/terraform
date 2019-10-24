065ac03433a67e4803fd2bebcf3a8f00bbe43925
Patrick Roughan <padraig.oruchain@gmail.com>
2019-10-24 09:29:30 +0000

    expand volumes

--- elk-ecs/elasticsearch.tf
+++ elk-ecs/elasticsearch.tf
@@ -76,7 +76,7 @@ resource "aws_ecs_service" "elasticsearch-service" {
   name            = "elasticsearch"
   cluster         = aws_ecs_cluster.elk-cluster.id
   task_definition = aws_ecs_task_definition.elasticsearch-task-definition.arn
-  desired_count   = 1
+  desired_count   = 2
   iam_role        = aws_iam_role.ecs-service-role.arn
   depends_on      = [aws_iam_policy_attachment.ecs-service-attach1]
 
