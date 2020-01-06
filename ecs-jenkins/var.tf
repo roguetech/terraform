f085169883128899c793cd29e577ab50e3e01945
patrick.roughan <patrick.roughan@visma.com>
2020-01-06 09:42:19 +0000

    ecs-jenkins

---
+++ ecs-jenkins/var.tf
@@ -0,0 +1,14 @@
+variable "AWS_REGION" {
+        default = "eu-west-1"
+}
+
+variable "PATH_TO_PRIVATE_KEY" {
+        default = "mykey"
+}
+
+variable "PATH_TO_PUBLIC_KEY" {
+        default = "mykey.pub"
+}
+
+#variable "AWS_ACCESS_KEY" {}
+#variable "AWS_SECRET_KEY" {}
