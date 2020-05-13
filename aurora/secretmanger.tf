bafc0aeae28e98debea16883f02858bd6bd9b920
patrick.roughan <patrick.roughan@visma.com>
2020-05-13 17:51:00 +0000

    adding restore cloudwatch

--- aurora/secretmanger.tf
+++ aurora/secretmanger.tf
@@ -1,9 +1,8 @@
-resource "aws_secretsmanager_secret" "test_password" {
-  name = "test_serverless_rds"
+resource "aws_secretsmanager_secret" "test_password2" {
+  name = "test2_serverless_rds"
 }
 
-resource "aws_secretsmanager_secret_version" "test_password" {
-  secret_id = "${aws_secretsmanager_secret.test_password.name}"
-  secret_string = ""
+resource "aws_secretsmanager_secret_version" "test_password2" {
+  secret_id = "${aws_secretsmanager_secret.test_password2.name}"
+  secret_string = "${var.aurora_password}"
 }
-
--- basic-instance/instance.tf
+++ basic-instance/instance.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "example" {
   ami           = "${lookup(var.AMIS, var.AWS_REGION)}"
   instance_type = "t2.micro"
-  subnet_id     = "subnet-6e619d09"
+  subnet_id     = "subnet-013841d90e7aa33ef"
 }
